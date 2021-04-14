bdc4b62afe28cfd74ca875b7af57c099dc98db0c
madhuri <madhuri.djs3@gmail.com>
2021-04-15 00:40:19 +0530

    add todo and delete todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+function App(){
+  const [task, setTask] = useState('');
+  const[taskList, setTaskList] = useState([]);
+
+  const handleChange = (event) => {
+    setTask(event.target.value);
+    console.log(task);
+    
+  };
+
+  const deleteTodo = (event, id) => {
+    event.preventDefault();
+    setTaskList(taskList.filter((t) => t.id !== id));
+  };
+
+  const addTodo = () => {
+    
+    if(task !== ""){
+      const taskDetails = {
+        id: Math.floor(Math.random()*1000),
+        value: task,
+      }
+      setTaskList([...taskList, taskDetails]);
+      document.querySelector(".input").value="";
+    }
+  };
+  console.log("taskList", taskList);
+  return(
+    <div>
+      <h1>Todo</h1>
+      <div>
+        <input type= "text"
+          name="text"
+          id= "text"   
+          className= "input"
+          onChange={(event) => handleChange(event)} />
+        <button className= "submit" onClick = {addTodo}>Submit</button>
+        {taskList !== [] ? (
+          <ul>
+           {taskList.map((t) => (
+              <li>{t.value}
+              <button className= "delete" onClick= {(event) => deleteTodo(event, t.id)}>delete</button></li>
+           ))}
+           
+          </ul>
+        ): null}
+      </div>  
+    </div>
+   
+  )
+}
 export default App;
